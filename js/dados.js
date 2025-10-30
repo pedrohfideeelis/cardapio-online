e8bedddefc91ab2ea4a2422cdc68cfa7a3a33083
pedrohfideeelis <phzimnerdola@gmail.com>
2025-10-30 12:19:29 -0300

    update images

--- js/dados.js
+++ js/dados.js
@@ -1,234 +1,234 @@
 var MENU = {
-    linhaEquilibrio: [
-        {
-            "id": "linha-equilibrio-frango-arroz-integral",
-            "img": "./img/burguer.png",
-            "name": "Frango com Arroz Integral",
-            "dsc": "Linha Equilíbrio — Marmita de 400g",
-            "price": 24.9
-        },
-        {
-            "id": "linha-equilibrio-frango-pure-batata-doce",
-            "img": "./img/burguer.png",
-            "name": "Frango com Purê de Batata Doce",
-            "dsc": "Linha Equilíbrio — Marmita de 400g",
-            "price": 24.9
-        },
-        {
-            "id": "linha-equilibrio-ragu-frango-arroz-integral",
-            "img": "./img/burguer.png",
-            "name": "Ragu de Frango com Arroz Integral",
-            "dsc": "Linha Equilíbrio — Marmita de 400g",
-            "price": 24.9
-        },
-        {
-            "id": "linha-equilibrio-ragu-frango-pure-doce",
-            "img": "./img/burguer.png",
-            "name": "Ragu de Frango com Purê Doce",
-            "dsc": "Linha Equilíbrio — Marmita de 400g",
-            "price": 24.9
-        },
-        {
-            "id": "linha-equilibrio-picadinho-arroz-integral",
-            "img": "./img/burguer.png",
-            "name": "Picadinho com Arroz Integral",
-            "dsc": "Linha Equilíbrio — Marmita de 400g",
-            "price": 24.9
-        },
-        {
-            "id": "linha-equilibrio-picadinho-pure-doce",
-            "img": "./img/burguer.png",
-            "name": "Picadinho com Purê Doce",
-            "dsc": "Linha Equilíbrio — Marmita de 400g",
-            "price": 24.9
-        },
-        {
-            "id": "linha-equilibrio-carne-moida-arroz-integral",
-            "img": "./img/burguer.png",
-            "name": "Carne Moída com Arroz Integral",
-            "dsc": "Linha Equilíbrio — Marmita de 400g",
-            "price": 24.9
-        },
-        {
-            "id": "linha-equilibrio-carne-moida-pure-doce",
-            "img": "./img/burguer.png",
-            "name": "Carne Moída com Purê Doce",
-            "dsc": "Linha Equilíbrio — Marmita de 400g",
-            "price": 24.9
-        },
-        {
-            "id": "linha-equilibrio-almondega-arroz-integral",
-            "img": "./img/burguer.png",
-            "name": "Almôndega com Arroz Integral",
-            "dsc": "Linha Equilíbrio — Marmita de 400g",
-            "price": 24.9
-        },
-        {
-            "id": "linha-equilibrio-almondega-pure-doce",
-            "img": "./img/burguer.png",
-            "name": "Almôndega com Purê Doce",
-            "dsc": "Linha Equilíbrio — Marmita de 400g",
-            "price": 24.9
-        }
-    ],
-    linhaDiaADia: [
-        {
-            "id": "linha-dia-a-dia-frango-arroz-feijao",
-            "img": "./img/burguer.png",
-            "name": "Frango com Arroz e Feijão",
-            "dsc": "Linha Dia a Dia — Marmita de 500g",
-            "price": 24.9
-        },
-        {
-            "id": "linha-dia-a-dia-frango-pure-legumes",
-            "img": "./img/burguer.png",
-            "name": "Frango com Purê e Legumes",
-            "dsc": "Linha Dia a Dia — Marmita de 500g",
-            "price": 24.9
-        },
-        {
-            "id": "linha-dia-a-dia-ragu-frango-arroz-feijao",
-            "img": "./img/burguer.png",
-            "name": "Ragu de Frango com Arroz e Feijão",
-            "dsc": "Linha Dia a Dia — Marmita de 500g",
-            "price": 24.9
-        },
-        {
-            "id": "linha-dia-a-dia-ragu-pure-legumes",
-            "img": "./img/burguer.png",
-            "name": "Ragu com Purê e Legumes",
-            "dsc": "Linha Dia a Dia — Marmita de 500g",
-            "price": 24.9
-        },
-        {
-            "id": "linha-dia-a-dia-picadinho-arroz-feijao",
-            "img": "./img/burguer.png",
-            "name": "Picadinho com Arroz e Feijão",
-            "dsc": "Linha Dia a Dia — Marmita de 500g",
-            "price": 24.9
-        },
-        {
-            "id": "linha-dia-a-dia-picadinho-pure-legumes",
-            "img": "./img/burguer.png",
-            "name": "Picadinho com Purê e Legumes",
-            "dsc": "Linha Dia a Dia — Marmita de 500g",
-            "price": 24.9
-        },
-        {
-            "id": "linha-dia-a-dia-carne-moida-arroz-feijao",
-            "img": "./img/burguer.png",
-            "name": "Carne Moída com Arroz e Feijão",
-            "dsc": "Linha Dia a Dia — Marmita de 500g",
-            "price": 24.9
-        },
-        {
-            "id": "linha-dia-a-dia-carne-moida-pure-legumes",
-            "img": "./img/burguer.png",
-            "name": "Carne Moída com Purê e Legumes",
-            "dsc": "Linha Dia a Dia — Marmita de 500g",
-            "price": 24.9
-        },
-        {
-            "id": "linha-dia-a-dia-almondega-arroz-feijao",
-            "img": "./img/burguer.png",
-            "name": "Almôndega com Arroz e Feijão",
-            "dsc": "Linha Dia a Dia — Marmita de 500g",
-            "price": 24.9
-        },
-        {
-            "id": "linha-dia-a-dia-almondega-pure-batata",
-            "img": "./img/burguer.png",
-            "name": "Almôndega com Purê de Batata",
-            "dsc": "Linha Dia a Dia — Marmita de 500g",
-            "price": 24.9
-        },
-        {
-            "id": "linha-dia-a-dia-galinhada-funcional",
-            "img": "./img/burguer.png",
-            "name": "Galinhada Funcional",
-            "dsc": "Linha Dia a Dia — Marmita de 500g",
-            "price": 24.9
-        },
-        {
-            "id": "linha-dia-a-dia-carne-panela-arroz-feijao",
-            "img": "./img/burguer.png",
-            "name": "Carne de Panela com Arroz e Feijão",
-            "dsc": "Linha Dia a Dia — Marmita de 500g",
-            "price": 24.9
-        },
-        {
-            "id": "linha-dia-a-dia-file-maca-arroz-feijao",
-            "img": "./img/burguer.png",
-            "name": "Filé ao Molho de Maçã com Arroz e Feijão",
-            "dsc": "Linha Dia a Dia — Marmita de 500g",
-            "price": 24.9
-        }
-    ],
-    linhaGourmet: [
-        {
-            "id": "linha-gourmet-lasanha-bolonhesa",
-            "img": "./img/burguer.png",
-            "name": "Lasanha à Bolonhesa da Casa",
-            "dsc": "Linha Gourmet — Marmita de 500–600g",
-            "price": 26.9
-        },
-        {
-            "id": "linha-gourmet-lasanha-frango-cremosa",
-            "img": "./img/burguer.png",
-            "name": "Lasanha de Frango Cremosa",
-            "dsc": "Linha Gourmet — Marmita de 500–600g",
-            "price": 26.9
-        },
-        {
-            "id": "linha-gourmet-escondidinho-nordestino",
-            "img": "./img/burguer.png",
-            "name": "Escondidinho Nordestino (carne seca)",
-            "dsc": "Linha Gourmet — Marmita de 500–600g",
-            "price": 26.9
-        },
-        {
-            "id": "linha-gourmet-escondidinho-frango",
-            "img": "./img/burguer.png",
-            "name": "Escondidinho de Frango",
-            "dsc": "Linha Gourmet — Marmita de 500–600g",
-            "price": 26.9
-        },
-        {
-            "id": "linha-gourmet-torta-brocolis",
-            "img": "./img/burguer.png",
-            "name": "Torta de Brócolis",
-            "dsc": "Linha Gourmet — Marmita de 500–600g",
-            "price": 26.9
-        },
-        {
-            "id": "linha-gourmet-caldo-abobora-gengibre-gorgonzola",
-            "img": "./img/burguer.png",
-            "name": "Caldo de Abóbora, Gengibre e Gorgonzola",
-            "dsc": "Linha Gourmet — Marmita de 500–600g",
-            "price": 26.9
-        },
-        {
-            "id": "linha-gourmet-feijoada",
-            "img": "./img/burguer.png",
-            "name": "Feijoada Gourmet",
-            "dsc": "Linha Gourmet — Marmita de 500–600g",
-            "price": 26.9
-        },
-        {
-            "id": "linha-gourmet-espaguete-funcional-picadinho",
-            "img": "./img/burguer.png",
-            "name": "Espaguete Funcional com Picadinho de Alcatra",
-            "dsc": "Linha Gourmet — Marmita de 500–600g",
-            "price": 26.9
-        }
-    ],
-    combosEspeciais: [
-        {
-            "id": "combos-especiais-5-marmitas",
-            "img": "./img/burguer.png",
-            "name": "5 marmitas por R$ 99,90",
-            "dsc": "Combos Especiais",
-            "price": 99.9
-        }
-    ]
+  linhaEquilibrio: [
+    {
+      id: "linha-equilibrio-frango-arroz-integral",
+      img: "./img/cardapio/linha-equilibrio/frango-integral.png",
+      name: "Frango com Arroz Integral",
+      dsc: "Linha Equilíbrio — Marmita de 400g",
+      price: 24.9,
+    },
+    {
+      id: "linha-equilibrio-frango-pure-batata-doce",
+      img: "./img/cardapio/linha-equilibrio/frango-pure-doce.png",
+      name: "Frango com Purê de Batata Doce",
+      dsc: "Linha Equilíbrio — Marmita de 400g",
+      price: 24.9,
+    },
+    {
+      id: "linha-equilibrio-ragu-frango-arroz-integral",
+      img: "./img/cardapio/linha-equilibrio/ragu-frango-integral.png",
+      name: "Ragu de Frango com Arroz Integral",
+      dsc: "Linha Equilíbrio — Marmita de 400g",
+      price: 24.9,
+    },
+    {
+      id: "linha-equilibrio-ragu-frango-pure-doce",
+      img: "./img/cardapio/linha-equilibrio/ragu-frango-pure-doce.png",
+      name: "Ragu de Frango com Purê Doce",
+      dsc: "Linha Equilíbrio — Marmita de 400g",
+      price: 24.9,
+    },
+    {
+      id: "linha-equilibrio-picadinho-arroz-integral",
+      img: "./img/cardapio/linha-equilibrio/picadinho-bovino-integral.png",
+      name: "Picadinho com Arroz Integral",
+      dsc: "Linha Equilíbrio — Marmita de 400g",
+      price: 24.9,
+    },
+    {
+      id: "linha-equilibrio-picadinho-pure-doce",
+      img: "./img/cardapio/linha-equilibrio/picadinho-bovino-pure-doce.png",
+      name: "Picadinho com Purê Doce",
+      dsc: "Linha Equilíbrio — Marmita de 400g",
+      price: 24.9,
+    },
+    {
+      id: "linha-equilibrio-carne-moida-arroz-integral",
+      img: "./img/cardapio/linha-equilibrio/carne-moida-integral.png",
+      name: "Carne Moída com Arroz Integral",
+      dsc: "Linha Equilíbrio — Marmita de 400g",
+      price: 24.9,
+    },
+    {
+      id: "linha-equilibrio-carne-moida-pure-doce",
+      img: "./img/cardapio/linha-equilibrio/carne-moida-pure-doce.png",
+      name: "Carne Moída com Purê Doce",
+      dsc: "Linha Equilíbrio — Marmita de 400g",
+      price: 24.9,
+    },
+    {
+      id: "linha-equilibrio-almondega-arroz-integral",
+      img: "./img/cardapio/linha-equilibrio/almondegas-integral.png",
+      name: "Almôndega com Arroz Integral",
+      dsc: "Linha Equilíbrio — Marmita de 400g",
+      price: 24.9,
+    },
+    {
+      id: "linha-equilibrio-almondega-pure-doce",
+      img: "./img/cardapio/linha-equilibrio/almondegas-pure-doce.png",
+      name: "Almôndega com Purê Doce",
+      dsc: "Linha Equilíbrio — Marmita de 400g",
+      price: 24.9,
+    },
+  ],
+  linhaDiaADia: [
+    {
+      id: "linha-dia-a-dia-frango-arroz-feijao",
+      img: "./img/cardapio/linha-dia-dia/frango-arroz-feijao.png",
+      name: "Frango com Arroz e Feijão",
+      dsc: "Linha Dia a Dia — Marmita de 500g",
+      price: 24.9,
+    },
+    {
+      id: "linha-dia-a-dia-frango-pure-legumes",
+      img: "./img/cardapio/linha-dia-dia/frango-pure.png",
+      name: "Frango com Purê e Legumes",
+      dsc: "Linha Dia a Dia — Marmita de 500g",
+      price: 24.9,
+    },
+    {
+      id: "linha-dia-a-dia-ragu-frango-arroz-feijao",
+      img: "./img/cardapio/linha-dia-dia/ragu-arroz-feijao.png",
+      name: "Ragu de Frango com Arroz e Feijão",
+      dsc: "Linha Dia a Dia — Marmita de 500g",
+      price: 24.9,
+    },
+    {
+      id: "linha-dia-a-dia-ragu-pure-legumes",
+      img: "./img/cardapio/linha-dia-dia/ragu-pure.png",
+      name: "Ragu com Purê e Legumes",
+      dsc: "Linha Dia a Dia — Marmita de 500g",
+      price: 24.9,
+    },
+    {
+      id: "linha-dia-a-dia-picadinho-arroz-feijao",
+      img: "./img/cardapio/linha-dia-dia/picadinho-bovino-arroz.png",
+      name: "Picadinho com Arroz e Feijão",
+      dsc: "Linha Dia a Dia — Marmita de 500g",
+      price: 24.9,
+    },
+    {
+      id: "linha-dia-a-dia-picadinho-pure-legumes",
+      img: "./img/cardapio/linha-dia-dia/picadinho-pure.png",
+      name: "Picadinho com Purê e Legumes",
+      dsc: "Linha Dia a Dia — Marmita de 500g",
+      price: 24.9,
+    },
+    {
+      id: "linha-dia-a-dia-carne-moida-arroz-feijao",
+      img: "./img/cardapio/linha-dia-dia/carne-moida-arroz.png",
+      name: "Carne Moída com Arroz e Feijão",
+      dsc: "Linha Dia a Dia — Marmita de 500g",
+      price: 24.9,
+    },
+    {
+      id: "linha-dia-a-dia-carne-moida-pure-legumes",
+      img: "./img/cardapio/linha-dia-dia/carne-moida-pure.png",
+      name: "Carne Moída com Purê e Legumes",
+      dsc: "Linha Dia a Dia — Marmita de 500g",
+      price: 24.9,
+    },
+    {
+      id: "linha-dia-a-dia-almondega-arroz-feijao",
+      img: "./img/cardapio/linha-dia-dia/frango-integral",
+      name: "Almôndega com Arroz e Feijão",
+      dsc: "Linha Dia a Dia — Marmita de 500g",
+      price: 24.9,
+    },
+    {
+      id: "linha-dia-a-dia-almondega-pure-batata",
+      img: "./img/cardapio/linha-dia-dia/frango-integral",
+      name: "Almôndega com Purê de Batata",
+      dsc: "Linha Dia a Dia — Marmita de 500g",
+      price: 24.9,
+    },
+    {
+      id: "linha-dia-a-dia-galinhada-funcional",
+      img: "./img/cardapio/linha-dia-dia/frango-integral",
+      name: "Galinhada Funcional",
+      dsc: "Linha Dia a Dia — Marmita de 500g",
+      price: 24.9,
+    },
+    {
+      id: "linha-dia-a-dia-carne-panela-arroz-feijao",
+      img: "./img/cardapio/linha-dia-dia/frango-integral",
+      name: "Carne de Panela com Arroz e Feijão",
+      dsc: "Linha Dia a Dia — Marmita de 500g",
+      price: 24.9,
+    },
+    {
+      id: "linha-dia-a-dia-file-maca-arroz-feijao",
+      img: "./img/cardapio/linha-dia-dia/frango-integral",
+      name: "Filé ao Molho de Maçã com Arroz e Feijão",
+      dsc: "Linha Dia a Dia — Marmita de 500g",
+      price: 24.9,
+    },
+  ],
+  linhaGourmet: [
+    {
+      id: "linha-gourmet-lasanha-bolonhesa",
+      img: "./img/cardapio/linha-equilibrio/frango-integral",
+      name: "Lasanha à Bolonhesa da Casa",
+      dsc: "Linha Gourmet — Marmita de 500–600g",
+      price: 26.9,
+    },
+    {
+      id: "linha-gourmet-lasanha-frango-cremosa",
+      img: "./img/cardapio/linha-equilibrio/frango-integral",
+      name: "Lasanha de Frango Cremosa",
+      dsc: "Linha Gourmet — Marmita de 500–600g",
+      price: 26.9,
+    },
+    {
+      id: "linha-gourmet-escondidinho-nordestino",
+      img: "./img/cardapio/linha-equilibrio/frango-integral",
+      name: "Escondidinho Nordestino (carne seca)",
+      dsc: "Linha Gourmet — Marmita de 500–600g",
+      price: 26.9,
+    },
+    {
+      id: "linha-gourmet-escondidinho-frango",
+      img: "./img/cardapio/linha-equilibrio/frango-integral",
+      name: "Escondidinho de Frango",
+      dsc: "Linha Gourmet — Marmita de 500–600g",
+      price: 26.9,
+    },
+    {
+      id: "linha-gourmet-torta-brocolis",
+      img: "./img/cardapio/linha-equilibrio/frango-integral",
+      name: "Torta de Brócolis",
+      dsc: "Linha Gourmet — Marmita de 500–600g",
+      price: 26.9,
+    },
+    {
+      id: "linha-gourmet-caldo-abobora-gengibre-gorgonzola",
+      img: "./img/cardapio/linha-equilibrio/frango-integral",
+      name: "Caldo de Abóbora, Gengibre e Gorgonzola",
+      dsc: "Linha Gourmet — Marmita de 500–600g",
+      price: 26.9,
+    },
+    {
+      id: "linha-gourmet-feijoada",
+      img: "./img/cardapio/linha-equilibrio/frango-integral",
+      name: "Feijoada Gourmet",
+      dsc: "Linha Gourmet — Marmita de 500–600g",
+      price: 26.9,
+    },
+    {
+      id: "linha-gourmet-espaguete-funcional-picadinho",
+      img: "./img/cardapio/linha-equilibrio/frango-integral",
+      name: "Espaguete Funcional com Picadinho de Alcatra",
+      dsc: "Linha Gourmet — Marmita de 500–600g",
+      price: 26.9,
+    },
+  ],
+  combosEspeciais: [
+    {
+      id: "combos-especiais-5-marmitas",
+      img: "./img/cardapio/linha-equilibrio/frango-integral",
+      name: "5 marmitas por R$ 99,90",
+      dsc: "Combos Especiais",
+      price: 99.9,
+    },
+  ],
 };
